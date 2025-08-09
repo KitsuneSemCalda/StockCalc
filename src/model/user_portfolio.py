b484173645ade829f3c164e862f5d5149184634a
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-08-09 00:04:13 -0300

    refactoring (model/user_portfolio): use the 'snake_case' to follow pep8

--- src/model/user_portfolio.py
+++ src/model/user_portfolio.py
@@ -1,14 +1,25 @@
 from typing import List
 from dataclasses import dataclass
-import financedatabase as fd
 
 
 @dataclass
 class UserPortfolio:
-    currencies: List[fd.Currencies]
-    etf: List[fd.ETFs]
-    equities: List[fd.Equities]
-    funds: List[fd.Funds]
-    cryptos: List[fd.Cryptos]
-    moneymarkets: List[fd.Moneymarkets]
-    indices: List[fd.Indices]
+    currencies: List[str]
+    etf: List[str]
+    equities: List[str]
+    funds: List[str]
+    cryptos: List[str]
+    moneymarkets: List[str]
+    indices: List[str]
+
+
+@dataclass
+class UserInfo:
+    # Initial quantity of value add to be balanced in simulation
+    initial_money_amount: int  # in cents
+    # The best chance of become Rich, BET on finding a unicorn
+    explosion_portfolio: UserPortfolio
+    # Trying extract a big montant or return, keep Betting
+    agressive_portfolio: UserPortfolio
+    # Don't be stupid and create a reserve of value.
+    conservative_portfolio: UserPortfolio
